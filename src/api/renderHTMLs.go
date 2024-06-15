340e71b33d043f263ba77eeacb535d6b58ef8e56
Lucas Bastino <lucasbastino.dev@gmail.com>
2024-06-15 13:58:38 -0300

    renderform funcionando

--- src/api/renderHTMLs.go
+++ src/api/renderHTMLs.go
@@ -9,6 +9,13 @@ import (
 	"github.com/LucasBastino/app-sindicato/src/models"
 )
 
+type Info struct {
+	Action string
+	Member models.Member
+}
+
+var funcMap = template.FuncMap{"ShowOrNot": ShowOrNot}
+
 func (c *Controller) renderHome(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("src/views/home.html"))
 	tmpl.Execute(w, nil)
@@ -73,35 +80,30 @@ func (c *Controller) renderMemberList(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *Controller) renderCreateMemberForm(w http.ResponseWriter, req *http.Request) {
-	// tmpl := createTemplate("src/views/createMemberForm.html")
-	// execTemplate(w, nil, tmpl, "createMemberForm.html")
-
-	// tmpl, _ := template.New("createMemberForm.html").ParseFiles("src/views/createMemberForm.html")
-	// tmpl.Execute(w, nil)
-
-	type Info struct {
-		Action string
+	// creo el template para crear un afiliado y lo ejecuto
+	tmpl, err := template.New("memberForm.html").Funcs(funcMap).ParseFiles("src/views/memberForm.html")
+	if err != nil {
+		fmt.Println("error parsing file memberForm.html")
 	}
-
-	tmpl, _ := template.ParseFiles("src/views/memberForm.html", "src/views/footer.html")
+	// tmpl, _ := template.ParseFiles("src/views/memberForm.html", "src/views/footer.html")
 	// el primero siempre es el main template, los demas se usan como componentes
-	tmpl.Execute(w, Info{"create"})
+	tmpl.Execute(w, Info{"create", models.Member{}}) // le paso un miembro vacio
 }
 
 func (c *Controller) renderEditMemberForm(w http.ResponseWriter, r *http.Request) {
-	type Info struct {
-		Action string
-		Member models.Member
-	}
+
 	var memberToEdit models.Member
 
+	// capto el param id de la URL
 	IdMember := r.PathValue("IdMember")
-	fmt.Println("member id:", IdMember)
+
+	// busco el miembro por id en la base de datos
 	result, err := c.DB.Query(fmt.Sprintf("SELECT * FROM MemberTable WHERE IdMember = '%s'", IdMember))
 	if err != nil {
 		fmt.Println("error searching member")
 	}
 
+	// creo una instancia de member a la que le atribuyo la informacion de la DB
 	for result.Next() {
 		err := result.Scan(&memberToEdit.IdMember, &memberToEdit.Name, &memberToEdit.DNI)
 		if err != nil {
@@ -110,6 +112,27 @@ func (c *Controller) renderEditMemberForm(w http.ResponseWriter, r *http.Request
 		}
 	}
 
-	tmpl := returnHtmlTemplate("src/views/memberForm.html")
+	// creo el template, le paso sus funciones y lo ejecuto
+	tmpl, err := template.New("memberForm.html").Funcs(funcMap).ParseFiles("src/views/memberForm.html")
+	if err != nil {
+		fmt.Println("error parsing file memberForm.html")
+	}
 	tmpl.Execute(w, Info{"edit", memberToEdit})
+}
+
+func ShowOrNot(info Info, field string) string {
+	if info.Action == "edit" {
+		switch field {
+		case "Name":
+			return info.Member.Name
+		case "DNI":
+			return info.Member.DNI
+		default:
+			return "error no field"
+		}
+	} else if info.Action == "create" {
+		return ""
+	} else {
+		return "big error"
+	}
 }
