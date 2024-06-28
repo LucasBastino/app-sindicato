6ca4f772631ad3317526e99b6a0a3395137c8814
unknown <lucasbastino.dev@gmail.com>
2024-06-28 13:11:23 -0300

    erross

--- src/api/controller.go
+++ src/api/controller.go
@@ -41,12 +41,14 @@ func (c *Controller) createMember(w http.ResponseWriter, r *http.Request) {
 
 	insert, err := c.DB.Query(fmt.Sprintf("INSERT INTO Membe2rTable (Name, DNI) VALUES ('%s','%s')", newMember.Name, newMember.DNI))
 	if err != nil {
-		DBError{"INSERT", "MEMB222ER"}.Error(err)
+		DBError{"INSERT MEMBER"}.Error(err)
 	}
 	defer insert.Close()
 
 	tmpl, err := template.ParseFiles(path)
-	checkError(err, "tmplError", path, models.DBError{})
+	if err != nil {
+		TmplError{path}.Error(err)
+	}
 	tmpl.Execute(w, newMember)
 	// http.Redirect(w, r, "/index", http.StatusSeeOther) // poner un status de redirect (30X), sino no funciona
 	// c.renderMemberList(w, r) // esto tambien funciona
@@ -56,7 +58,9 @@ func (c *Controller) deleteMember(w http.ResponseWriter, r *http.Request) {
 	IdMember := r.PathValue("IdMember")
 	fmt.Println(IdMember)
 	delete, err := c.DB.Query(fmt.Sprintf("DELETE FROM MemberTable WHERE IdMember = '%s'", IdMember))
-	checkError(err, "dbError", "", models.DBError{Statement: "DELETE", Model: "MEMBER"})
+	if err != nil {
+		DBError{"DELETE MEMBER"}.Error(err)
+	}
 	delete.Close()
 
 	c.renderMemberTable(w, r)
@@ -66,7 +70,9 @@ func (c *Controller) editMember(w http.ResponseWriter, r *http.Request) {
 	memberEdited := parseMember(r)
 	IdMember := r.PathValue("IdMember")
 	update, err := c.DB.Query(fmt.Sprintf("UPDATE MemberTable SET Name = '%s', DNI = '%s' WHERE IdMember = '%s'", memberEdited.Name, memberEdited.DNI, IdMember))
-	checkError(err, "dbError", "", models.DBError{Statement: "UPDATE", Model: "MEMBER"})
+	if err != nil {
+		DBError{"UPDATE MEMBER"}.Error(err)
+	}
 	update.Close()
 	// no puedo hacer esto ↓ porque estoy en POST, no puedo redireccionar
 	http.Redirect(w, r, "/index", http.StatusSeeOther) // con este status me anda, con otros de 300 no
@@ -78,7 +84,9 @@ func (c *Controller) editParent(w http.ResponseWriter, r *http.Request) {
 	Rel := r.FormValue("rel")
 
 	update, err := c.DB.Query(fmt.Sprintf("UPDATE ParentTable SET Name = '%s', Rel = '%s' WHERE IdParent = '%s'", Name, Rel, IdParent))
-	checkError(err, "dbError", "", models.DBError{Statement: "UPDATE", Model: "PARENT"})
+	if err != nil {
+		DBError{"UPDATE PARENT"}.Error(err)
+	}
 	update.Close()
 
 	c.renderParentFile(w, r)
@@ -92,12 +100,16 @@ func (c *Controller) createEnterprise(w http.ResponseWriter, r *http.Request) {
 
 	insert, err := c.DB.Query(fmt.Sprintf("INSERT INTO EnterpriseTable (Name, Address) VALUES ('%s', '%s')", enterprise.Name, enterprise.Address))
 	fmt.Println("error inserting data to database")
-	checkError(err, "dbError", "", models.DBError{Statement: "INSERT", Model: "ENTERPRISE"})
+	if err != nil {
+		DBError{"INSERT ENTERPRISE"}.Error(err)
+	}
 	defer insert.Close()
 
 	path := "src/views/files/enterpriseFile.html"
 	tmpl, err := template.ParseFiles(path)
-	checkError(err, "tmplError", path, models.DBError{})
+	if err != nil {
+		TmplError{path}.Error(err)
+	}
 	tmpl.Execute(w, enterprise)
 
 }
@@ -108,17 +120,23 @@ func (c *Controller) searchMember(w http.ResponseWriter, r *http.Request) {
 	var member models.Member
 
 	result, err := c.DB.Query(fmt.Sprintf(`SELECT * FROM MemberTable WHERE Name LIKE '%%%s%%' OR DNI LIKE '%%%s%%'`, searchKey, searchKey))
-	checkError(err, "dbError", "", models.DBError{Statement: "SELECT", Model: "MEMBER"})
+	if err != nil {
+		DBError{"SELECT MEMBER"}.Error(err)
+	}
 	for result.Next() {
 		err = result.Scan(&member.IdMember, &member.Name, &member.DNI)
-		checkError(err, "scanError", "", models.DBError{})
+		if err != nil {
+			ScanError{"MEMBER"}.Error(err)
+		}
 		members = append(members, member)
 	}
 	defer result.Close()
 
 	path := "src/views/tables/memberTable.html"
 	tmpl, err := template.ParseFiles(path)
-	checkError(err, "tmplError", path, models.DBError{})
+	if err != nil {
+		TmplError{path}.Error(err)
+	}
 	tmpl.Execute(w, members)
 }
 
@@ -128,17 +146,23 @@ func (c *Controller) searchEnterprise(w http.ResponseWriter, r *http.Request) {
 	var enterprise models.Enterprise
 
 	result, err := c.DB.Query(fmt.Sprintf(`SELECT * FROM EnterpriseTable WHERE Name LIKE '%%%s%%' OR Address LIKE '%%%s%%'`, searchKey, searchKey))
-	checkError(err, "dbError", "", models.DBError{Statement: "SELECT", Model: "ENTERPRISE"})
+	if err != nil {
+		DBError{"SELECT ENTERPRISE"}.Error(err)
+	}
 	for result.Next() {
 		err = result.Scan(&enterprise.IdEnterprise, &enterprise.Name, &enterprise.Address)
-		checkError(err, "scanError", "", models.DBError{})
+		if err != nil {
+			ScanError{"ENTERPRISE"}.Error(err)
+		}
 		enterprises = append(enterprises, enterprise)
 	}
 	defer result.Close()
 
 	path := "src/views/tables/enterpriseTable.html"
 	tmpl, err := template.ParseFiles(path)
-	checkError(err, "tmplError", path, models.DBError{})
+	if err != nil {
+		TmplError{path}.Error(err)
+	}
 	tmpl.Execute(w, enterprises)
 }
 
@@ -149,49 +173,47 @@ func (c *Controller) searchParent(w http.ResponseWriter, r *http.Request) {
 
 	result, err := c.DB.Query(fmt.Sprintf(`SELECT * FROM ParentTable WHERE Name LIKE '%%%s%%' OR Rel LIKE '%%%s%%'`, searchKey, searchKey))
 	if err != nil {
-		checkError(err, "dbError", "", models.DBError{Statement: "SELECT", Model: "PARENT"})
+		DBError{"SELECT PARENT"}.Error(err)
 	}
 	for result.Next() {
 		err = result.Scan(&parent.IdParent, &parent.Name, &parent.Rel, &parent.IdMember)
-		checkError(err, "scanError", "", models.DBError{})
+		if err != nil {
+			ScanError{"PARENT"}.Error(err)
+		}
 		parents = append(parents, parent)
 	}
 	defer result.Close()
 
 	path := "src/views/tables/allParentsTable.html"
 	tmpl, err := template.ParseFiles(path)
-	checkError(err, "tmplError", path, models.DBError{})
+	if err != nil {
+		TmplError{path}.Error(err)
+	}
 	tmpl.Execute(w, parents)
 
 }
 
-type ErrorInterface interface {
-	Error(err error)
-}
-
 type DBError struct {
-	Statement string
-	Model     string
+	Action string
 }
-
-func (db DBError) Error(err error) {
-	fmt.Printf("error with '%s %s' database action\n", db.Statement, db.Model)
-	panic(err)
-}
-
 type ScanError struct {
 	Model string
 }
-
-func (scan ScanError) Error(err error) {
-	fmt.Println("error scanning data from result to", scan.Model)
-}
-
 type TmplError struct {
 	Path string
 }
 
+func (db DBError) Error(err error) {
+	fmt.Printf("error with '%s %s' database action\n", db.Action)
+	panic(err)
+}
+
 func (tmpl TmplError) Error(err error) {
 	fmt.Printf("error parsing file '%s'\n", tmpl.Path)
+	panic(err)
+}
+
+func (scan ScanError) Error(err error) {
+	fmt.Println("error scanning data from result to", scan.Model)
 	panic(err)
 }
