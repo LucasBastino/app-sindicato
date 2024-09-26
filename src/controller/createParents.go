4f55379063f957d0358bd91a138c01b8c1765af5
unknown <lucasbastino.dev@gmail.com>
2024-09-26 10:01:13 -0300

    asdsd

--- src/controller/createParents.go
+++ src/controller/createParents.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/LucasBastino/app-sindicato/src/database"
+	"github.com/LucasBastino/app-sindicato/src/models"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -46,16 +47,6 @@ import (
 // }
 
 func CreateParents(c *fiber.Ctx) error {
-	type Parent struct {
-		IdParent int
-		Name     string
-		LastName string
-		Rel      string
-		Birthday string
-		Gender   string
-		CUIL     string
-		IdMember int
-	}
 	type JSONData struct {
 		MaleFirstNames   []string
 		FemaleFirstNames []string
@@ -63,7 +54,8 @@ func CreateParents(c *fiber.Ctx) error {
 		Genders          []string
 	}
 
-	p := Parent{}
+	p := models.Parent{}
+
 	file, err := os.Open("./data/jsonData.json")
 	if err != nil {
 		fmt.Println("error opening file")
@@ -72,83 +64,99 @@ func CreateParents(c *fiber.Ctx) error {
 	decoder := json.NewDecoder(file)
 	jsonData := JSONData{}
 	decoder.Decode(&jsonData)
-	p.IdParent = rand.IntN(3000)
-	p.IdMember = rand.IntN(200)
-	p.Gender = jsonData.Genders[rand.IntN(len(jsonData.Genders))]
-	if p.Gender == "Hombre" {
-		p.Name = jsonData.MaleFirstNames[rand.IntN(len(jsonData.MaleFirstNames))]
-		p.Rel = "Hijo"
-	} else if p.Gender == "Mujer" {
-		p.Name = jsonData.FemaleFirstNames[rand.IntN(len(jsonData.FemaleFirstNames))]
-		p.Rel = "Hija"
-	} else if p.Gender == "Otro" {
-		p.Rel = "Hijx"
-		random := rand.IntN(1)
-		if random == 0 {
-			p.Name = jsonData.MaleFirstNames[rand.IntN(len(jsonData.MaleFirstNames))]
-		} else {
-			p.Name = jsonData.FemaleFirstNames[rand.IntN(len(jsonData.FemaleFirstNames))]
-		}
-	}
-	p.LastName = jsonData.LastNames[rand.IntN(len(jsonData.LastNames))]
+
 	// BUSCAR LA MANERA DE QUE SOLO TE APAREZCAN LOS MENORES DE 50
 	// con birthday formato fecha y una funcion en SQL que saque el año y que busque que sea mayor a 1974
 	// tambien hacer que el form de fechas sea con 3 input text y que despues de ahi se pase a fecha en backend
 	// y que para mostrarlo se obtenga de la DB en formato fecha, en backend se pase a string y de ahi se renderiza en los 3 inputs
 	// o en 1 input pero separados con barras (no se como se hace)
-	result, err := database.DB.Query("SELECT IdMember, Birthday FROM MemberTable WHERE CAST(Birthday AS DATE) > '1990-01-01'")
+
+	// busco los afiliados menores de 50 años, esos van a tener hijos
+	result, err := database.DB.Query("SELECT IdMember, Birthday FROM MemberTable WHERE CAST(Birthday AS DATE) > '1974-01-01'")
 	if err != nil {
 		fmt.Println("error consulting member table")
 		panic(err)
 	}
-	var memberBirthday string
-	var memberMap map[int]string
-	var idMemberTemp int
-	var memberBirthdayTemp string
+
+	// creo un struct para facilitar la lectura de datos
+	type MemberDateInfo struct {
+		id       int
+		birthday string
+	}
+	var memberDateInfoList []MemberDateInfo
+	var memberDateInfo MemberDateInfo
+
+	// guardo los datos de cada afiliado en un slice
 	for result.Next() {
-		err = result.Scan(&idMemberTemp, &memberBirthdayTemp)
+		err = result.Scan(&memberDateInfo.id, &memberDateInfo.birthday)
 		if err != nil {
 			fmt.Println("error scanning member id and birthday")
 			panic(err)
 		}
-		fmt.Println(idMemberTemp, memberBirthdayTemp)
-		// memberMap[idMemberTemp] = memberBirthdayTemp
+		memberDateInfoList = append(memberDateInfoList, memberDateInfo)
 	}
 	result.Close()
-	largodelmapa := len(memberMap)
-	fmt.Println("largo del mapa", largodelmapa)
-	// consulto la fecha de nacimiento del afiliado mayor y obtengo el año
-	memberYearStr := memberBirthday[(len(memberBirthday) - 4):]
-	memberYear, err := strconv.Atoi(memberYearStr)
-	if err != nil {
-		fmt.Println("error converting memberYearStr to int")
-		panic(err)
-	}
-	fmt.Println(memberYear)
-	// la edad del hijo sera entre 0 y 25 años, por lo tanto va a nacer entre 1999 y 2024
-	year := rand.IntN(25) + 1999
-	ageDifference := year - memberYear
-	if ageDifference < 20 {
-		year = year + 20 - ageDifference
-	}
-	if year > 2024 {
-		year = 2024
-	} else if year < 1999 {
-		year = 1999
-	}
-	month := rand.IntN(11) + 1
-	var day int
-	switch month {
-	case 2:
-		day = rand.IntN(27) + 1
-	case 4, 6, 9, 11:
-		day = rand.IntN(29) + 1
-	case 1, 3, 5, 7, 8, 10, 12:
-		day = rand.IntN(30) + 1
-	}
-	// fijarse bien desp lo del formato fecha
-	p.Birthday = fmt.Sprintf("%d-%d-%d", day, month, year)
-	p.CUIL = fmt.Sprintf("%d-%s-%d", rand.IntN(9)+20, strconv.Itoa(rand.IntN(3000000)+2000000), rand.IntN(8)+1)
 
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"parent": p})
+	for i := 0; i < 500; i++ {
+		randomMember := memberDateInfoList[rand.IntN(len(memberDateInfoList))]
+		memberYearStr := randomMember.birthday[:4]
+		memberYear, err := strconv.Atoi(memberYearStr)
+		if err != nil {
+			fmt.Println("error converting memberYearStr to int")
+			panic(err)
+		}
+		p.IdMember = randomMember.id
+
+		// obtengo el año de nacimiento del afiliado mayor
+		// la edad del hijo sera entre 0 y 25 años, por lo tanto va a nacer entre 1999 y 2024
+		year := rand.IntN(25) + 1999
+		ageDifference := year - memberYear
+		if ageDifference < 20 {
+			year = year + 20 - ageDifference
+		}
+		if year > 2024 {
+			year = 2024
+		} else if year < 1999 {
+			year = 1999
+		}
+		month := rand.IntN(11) + 1
+		var day int
+		switch month {
+		case 2:
+			day = rand.IntN(27) + 1
+		case 4, 6, 9, 11:
+			day = rand.IntN(29) + 1
+		case 1, 3, 5, 7, 8, 10, 12:
+			day = rand.IntN(30) + 1
+		}
+		// fijarse bien desp lo del formato fecha
+		p.Birthday = fmt.Sprintf("%d-%d-%d", day, month, year)
+		p.CUIL = fmt.Sprintf("%d-%s-%d", rand.IntN(9)+20, strconv.Itoa(rand.IntN(3000000)+2000000), rand.IntN(8)+1)
+		p.Gender = jsonData.Genders[rand.IntN(len(jsonData.Genders))]
+		if p.Gender == "Hombre" {
+			p.Name = jsonData.MaleFirstNames[rand.IntN(len(jsonData.MaleFirstNames))]
+			p.Rel = "Hijo"
+		} else if p.Gender == "Mujer" {
+			p.Name = jsonData.FemaleFirstNames[rand.IntN(len(jsonData.FemaleFirstNames))]
+			p.Rel = "Hija"
+		} else if p.Gender == "Otro" {
+			p.Rel = "Hijx"
+			random := rand.IntN(1)
+			if random == 0 {
+				p.Name = jsonData.MaleFirstNames[rand.IntN(len(jsonData.MaleFirstNames))]
+			} else {
+				p.Name = jsonData.FemaleFirstNames[rand.IntN(len(jsonData.FemaleFirstNames))]
+			}
+		}
+		p.LastName = jsonData.LastNames[rand.IntN(len(jsonData.LastNames))]
+
+		insert, err := database.DB.Query(fmt.Sprintf("INSERT INTO ParentTable (Name, LastName, Rel, Birthday, Gender, CUIL, IdMember) VALUES ('%s','%s', '%s', '%s', '%s', '%s', '%d')", p.Name, p.LastName, p.Rel, p.Birthday, p.Gender, p.CUIL, p.IdMember))
+		if err != nil {
+			fmt.Println("error inserting parent")
+			panic(err)
+		}
+		insert.Close()
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "parents added"})
+
 }
