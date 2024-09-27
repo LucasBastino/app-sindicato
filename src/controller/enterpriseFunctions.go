1914351ee770ea9a27e6f232d5f777851c9a19a8
unknown <lucasbastino.dev@gmail.com>
2024-09-27 15:52:50 -0300

    asdds

--- src/controller/enterpriseFunctions.go
+++ src/controller/enterpriseFunctions.go
@@ -143,7 +143,7 @@ func RenderEnterpriseMembers(c *fiber.Ctx) error {
 
 		// creo un map con todas las variables
 		data := getFiberMapCaller(models.Member{}, members, searchKey, currentPage, someBefore, someAfter, totalPages, totalPagesArray)
-
+		data["mode"] = "enterpriseMemberTable"
 		// renderizo la tabla y le envio el map con las variables
 		return c.Render("memberTable", data)
 	}
